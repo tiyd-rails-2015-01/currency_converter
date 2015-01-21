160dedf5dc9893d376f6c075c3ff707d9e313da3
C Haden <chaden@spoonflower.com>
2015-01-21 13:29:20 -0500

    has test to check that CurrencyConverter is initialized with a Hash of currency codes to conversion rates

--- currency_test.rb
+++ currency_test.rb
@@ -1,6 +1,7 @@
 require 'minitest/autorun'
 require 'minitest/pride'
 require './currency.rb'
+# Day 1 Requirements:
 
 # Currency:
 #
@@ -10,6 +11,35 @@ require './currency.rb'
 # * Should be able to be subtracted by another Currency object with the same currency code
 # * Should raise a DifferentCurrencyCodeError when you try to add or subtract two Currency objects with different currency codes.
 # * Should be able to be multiplied by a Fixnum or Float and return a Currency object
+
+#/-----------------------------------------------
+# Day Two Requirements:
+
+# CurrencyConverter:
+#
+# Should be initialized with a Hash of currency codes to conversion rates (see link to rates below)
+# At first, just make this work with two currency codes and conversation rates, with one rate being
+# 1.0 and the other being the conversation rate. An example would be this: {USD: 1.0, EUR: 0.74},
+# which implies that a dollar is worth 0.74 euros.
+# Should be able to take a Currency object and a requested currency code that is the same currency
+# code as the Currency object's and return a Currency object equal to the one passed in (that is,
+# currency_converter.convert(Currency.new(1, :USD), :USD) == Currency.new(1, :USD))
+# Should be able to take a Currency object that has one currency code it knows and a requested
+# currency code and return a new Currency object with the right amount in the new currency code
+# Should be able to be created with a Hash of three or more currency codes and conversion rates.
+# An example would be this: {USD: 1.0, EUR: 0.74, JPY: 120.0}, which implies that a dollar is
+# worth 0.74 euros and that a dollar is worth 120 yen, but also that a euro is worth 120/0.74 =
+# 162.2 yen.
+# Should be able to convert Currency in any currency code it knows about to Currency in any other
+# currency code it knows about.
+# Should raise an UnknownCurrencyCodeError when you try to convert from or to a currency code it
+# doesn't know about.
+#
+# Currency (modifications to earlier code):
+#
+# Currency.new should be able to take one argument with a currency symbol embedded in it, like
+# "$1.20" or "€ 7.00", and figure out the correct currency code. It can also take two arguments
+# like before, one being the amount and the other being the currency code.
 
 class CurrencyTest < Minitest::Test
 
@@ -95,6 +125,11 @@ class CurrencyTest < Minitest::Test
     assert_raises( UnsupportedCurrencyCodeError) do
       fake - euro1
     end
+  end
+
+  def test_currency_converter_initialized_with_conversion_rates
+    converter = CurrencyConverter.new
+    assert_equal Hash, converter.conversion_rates.class
   end
 
 end
