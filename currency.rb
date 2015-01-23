2aceed16f58dc40fd51f8dd30f4a78d6084539f8
Topstitch <kallen@spoonflower.com>
2015-01-23 12:05:48 -0500

    Initial commit

---
+++ currency.rb
@@ -0,0 +1,35 @@
+require './different_currency_code_error'
+
+
+class Currency
+  attr_reader :amount, :code
+  def initialize(amount, code)
+    @amount = amount
+    @code = currency_code
+  end
+
+  def == (other_money)
+    self.amount == other_money.amount && self.code == other_money.code
+  end
+
+  def + (other_money)
+    if self.code == other_money.code
+      Currency.new(amount + other_money.amount, code)
+    else
+      raise DifferentCurrencyCodeError, "You cannot add two different kinds of currency"
+    end
+  end
+
+  def - (other_money)
+    if self.code == other_money.code
+      Currency.new(amount - other_money.amount, code)
+    else
+      raise DifferentCurrencyCodeError, "You cannot subtract two different kinds of currency."
+    end
+  end
+
+  def * (number)
+    Currency.new(amount * number, code)
+  end
+
+end
