835e57f0c0859d4ebbab5a71be74494d3e09c766
Topstitch <kallen@spoonflower.com>
2015-01-24 17:04:47 -0500

    Final version

--- goat_money_machine.rb
+++ goat_money_machine.rb
@@ -3,13 +3,10 @@ require './unknown_currency_code_error'
 require './currency'
 require './currency_converter'
 
-
-
-
-
 def change_real_money(mm)
   array = mm.symbols.invert.keys
   symbols_to_show = array.reduce(' ') {|start, each| start += each.to_s + '  '}
+  #this lovely reduce business comes courtesy of Michael Byrd
   puts "How much money do you have? Please enter it with the proper symbol, e.g. $10.00.\nChoose from the following:#{symbols_to_show}"
   input = gets.chomp
   correct_input = false
@@ -21,7 +18,7 @@ def change_real_money(mm)
         puts "Please enter your value with the correct symbol."
         correct_input = false
         input = gets.chomp
-      end    
+      end
     end
   my_money = Currency.new(cash)
   symbol_array = mm.currency_codes.keys
