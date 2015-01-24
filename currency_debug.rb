9221a524de9576906a0705fbb549e84cf0c772c1
Topstitch <kallen@spoonflower.com>
2015-01-24 16:58:39 -0500

    Interactive currency converter works

--- currency_debug.rb
+++ currency_debug.rb
@@ -12,12 +12,17 @@ def change_real_money(mm)
   symbols_to_show = array.reduce(' ') {|start, each| start += each.to_s + '  '}
   puts "How much money do you have? Please enter it with the proper symbol, e.g. $10.00.\nChoose from the following:#{symbols_to_show}"
   input = gets.chomp
-  if array.include?(input[0])
-    cash = input
-  else
-    puts "Please enter your value with the correct symbol."
-    input = gets.chomp
-  end
+  correct_input = false
+    while correct_input == false
+      if array.include?(input[0])
+        correct_input = true
+        cash = input
+      else
+        puts "Please enter your value with the correct symbol."
+        correct_input = false
+        input = gets.chomp
+      end    
+    end
   my_money = Currency.new(cash)
   symbol_array = mm.currency_codes.keys
   displayable_keys = symbol_array.reduce('| ') {|start, each| start += each.to_s + ' | '}
