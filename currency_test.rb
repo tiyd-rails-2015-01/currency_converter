141d727ae2bf9d40a5d8103ff45f5deac59c2e76
Maria <mliberov@gmail.com>
2015-01-25 17:42:18 -0500

    Add object addition test

--- currency_test.rb
+++ currency_test.rb
@@ -22,4 +22,12 @@ class CurrencyTest < Minitest::Test
     refute currency.amount == currency2.amount
     refute currency.amount == currency3.amount
   end
+  
+  def test_two_objects_with_same_currency_code_can_be_added
+    currency1 = Currency.new(12, :USD)
+    currency2 = Currency.new(15, :USD)
+    currency_total = currency1 + currency2
+    assert currency_total.amount == 27
+    assert currency_total.currency_code == :USD
+  end
 end
