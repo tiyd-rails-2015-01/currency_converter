b78ca9ae74bf189c90e0dcab7ee0500f4c97933c
Maria <mliberov@gmail.com>
2015-01-25 17:38:14 -0500

    Fix same currency amount and code should be equal test and code

--- currency_test.rb
+++ currency_test.rb
@@ -16,6 +16,10 @@ class CurrencyTest < Minitest::Test
   def test_equals_other_object_with_same_amount_and_currency_code
     currency = Currency.new(12, :USD)
     currency1 = Currency.new(12, :USD)
+    currency2 = Currency.new(15, :USD)
+    currency3 = Currency.new(41, :GBP)
     assert currency.amount == currency1.amount
+    refute currency.amount == currency2.amount
+    refute currency.amount == currency3.amount
   end
 end
