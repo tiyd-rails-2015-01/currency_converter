34f194ab005973b1bc5c7e1284b2832468ca0a98
Maria <mliberov@gmail.com>
2015-01-25 15:36:39 -0500

    Add created with amount and currency code test

--- currency_test.rb
+++ currency_test.rb
@@ -4,6 +4,12 @@ require './currency'
 
 class CurrencyTest < Minitest::Test
   def test_currency_class_exists
-    Currency
+    assert Currency
+  end
+
+  def test_created_with_amount_and_currency_code
+    currency = Currency.new(12, :USD)
+    assert currency.amount == 12
+    assert currency.currency_code == :USD
   end
 end
