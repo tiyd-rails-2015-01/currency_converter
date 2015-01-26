bc031f57d51878dd7e54a65fdd0acbfb6eb10bb6
Maria <mliberov@gmail.com>
2015-01-26 07:57:11 -0500

    Make currency objects are equal test pass

--- currency_converter.rb
+++ currency_converter.rb
@@ -4,4 +4,11 @@ class CurrencyConverter <Currency
   def initialize(conversion_rates)
     @conversion_rates = conversion_rates
   end
+
+  def convert(currency_object, code)
+    new_amount = (currency_object.amount /
+    conversion_rates[currency_object.currency_code]) *
+    conversion_rates[code]
+    Currency.new(new_amount, code)
+  end
 end
